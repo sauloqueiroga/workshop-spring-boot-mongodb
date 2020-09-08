dcf8458801a5662e8cc776c968303c5b98e57aa7
Saulo Queiroga <sauloerick@hotmail.com>
2020-09-07 22:49:12 -0300

    Obtendo um usuário por id

--- src/main/java/com/sauloqueiroga/mongo/services/UserService.java
+++ src/main/java/com/sauloqueiroga/mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.sauloqueiroga.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sauloqueiroga.mongo.domain.User;
 import com.sauloqueiroga.mongo.repository.UserRepository;
+import com.sauloqueiroga.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/sauloqueiroga/mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sauloqueiroga.mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);		
+	}
+}
