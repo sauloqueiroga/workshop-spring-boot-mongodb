b261ab786f9e4f44641667ab616a12fce004d31a
Saulo Queiroga <sauloerick@hotmail.com>
2020-09-09 17:16:48 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/sauloqueiroga/mongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.sauloqueiroga.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sauloqueiroga.mongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
