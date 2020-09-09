1c504b610cf57cbcb6f3b924ef14552237c48650
Saulo Queiroga <sauloerick@hotmail.com>
2020-09-09 18:04:40 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/sauloqueiroga/mongo/config/Instantiation.java
+++ src/main/java/com/sauloqueiroga/mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.sauloqueiroga.mongo.domain.Post;
 import com.sauloqueiroga.mongo.domain.User;
+import com.sauloqueiroga.mongo.dto.AuthorDTO;
 import com.sauloqueiroga.mongo.repository.PostRepository;
 import com.sauloqueiroga.mongo.repository.UserRepository;
 
@@ -33,12 +34,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");		
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abrços!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abrços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 	
